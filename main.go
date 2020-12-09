84c01df3106187e828bc8cce4f3e6a0a3929a39a
Adom Sackey <adom.466@gmail.com>
2020-12-09 09:01:52 +0000

    Initial Commit

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+  "encoding/json"
+  "github.com/gorilla/mux"
+  "io/ioutil"
+  "log"
+  "net/http"
+)
+
+
+// Structs
+type Bank struct {
+  Name string `json:"name"`
+  Address string `json:"address"`
+  PhoneNumber string `json:"number"`
+  Website string `json:"website"`
+  Email string `json:"email"`
+}
+
+type BankJSON struct {
+  Name string `json:"name"`
+  Address string `json:"address"`
+  PhoneNumber string `json:"number"`
+  Website string `json:"website"`
+  Email string `json:"email"`
+}
+
+var banks []BankJSON
+
+func getAllBanks(w http.ResponseWriter, r *http.Request)  {
+  w.Header().Set("Content-Type", "application/json")
+
+  var newBanks []Bank
+
+  for _, bank := range banks {
+    newBanks = append(newBanks, Bank{
+      Name: bank.Name,
+      Address: bank.Address,
+      PhoneNumber: bank.PhoneNumber,
+      Website: bank.Website,
+      Email: bank.Email,
+    })
+  }
+
+  _ = json.NewEncoder(w).Encode(newBanks)
+}
+
+func main() {
+
+  bankData, err := ioutil.ReadFile("./bankdata.json")
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  if err := json.Unmarshal(bankData, &banks); err != nil {
+    log.Fatal(err)
+  }
+
+  r := mux.NewRouter()
+
+  //Initialize Routes
+  r.HandleFunc("/banks", getAllBanks).Methods("GET")
+
+  // Start server
+  log.Fatal(http.ListenAndServe(":8000", r))
+}
